175db1bf75313146a25a292c62203353dd722505
choi <wjfekr1@naver.com>
2023-01-31 19:53:43 +0900

    코딩테스트

--- January/단어 변환.js
+++ January/단어 변환.js
@@ -1,6 +1,31 @@
 function solution(begin, target, words) {
-  console.log("d");
+  const visited = { [begin]: 0 };
+  const queue = [begin];
+  console.log(visited);
+
+  const isConnected = (str1, str2) => {
+    let cnt = str1.length;
+    for (let i = 0; i < str1.length; i++) {
+      if (str1[i] == str2[i]) {
+        cnt--;
+      }
+    }
+    return cnt == 1 ? true : false;
+  };
+
+  while (queue.length) {
+    const cur = queue.shift();
+    if (cur === target) break;
+
+    for (const word of words) {
+      if (isConnected(cur, word) && !visited[word]) {
+        queue.push(word);
+        visited[word] = visited[cur] + 1;
+      }
+    }
+  }
+  return visited[target] ? visited[target] : 0;
 }
 
-solution("hit", "cog", ["hot", "dot", "dog", "lot", "log", "cog"]);
-solution("hit", "cog", ["hot", "dot", "dog", "lot", "log"]);
+console.log(solution("hit", "cog", ["hot", "dot", "dog", "lot", "log", "cog"]));
+// solution("hit", "cog", ["hot", "dot", "dog", "lot", "log"]);
